f755776b0e364dd87b0c2a15edf3ad41d8200553
jacksonmcafee <jacksonmcafee1@gmail.com>
2024-01-04 18:24:13 -0500

    Added commands enum and map

---
+++ src/commands.cpp
@@ -0,0 +1,9 @@
+#include "commands.h"
+
+std::map<std::string, CommandType> commandMap = {
+    {"add", CommandType::ADD},
+    {"delete", CommandType::DELETE},
+    {"list", CommandType::LIST},
+    {"show", CommandType::SHOW},
+    {"quit", CommandType::QUIT},
+};
---
+++ src/commands.h
@@ -0,0 +1,18 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+#include <map>
+#include <string>
+
+enum class CommandType {
+    NONE,
+    ADD,
+    DELETE,
+    LIST,
+    SHOW,
+    QUIT,
+};
+
+extern std::map<std::string, CommandType> commandMap;
+
+#endif // COMMANDS_H
